2454338ddc7e50391ab16c2a4589bc69d4b0c4dd
Rifqi Pratama <rifqipratama9@gmail.com>
2022-10-27 15:00:47 +0700

    change dashboard admin language to english

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('title')
-    Get A Glance | Dashboard
+    {{ config('app.name') }} Admin | Home 
 @endsection
 
 @section('content')
@@ -10,11 +10,11 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Beranda</h1>
+                    <h1 class="m-0">Home</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item active">Beranda</li>
+                        <li class="breadcrumb-item active">Home</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
@@ -33,12 +33,12 @@
                         <div class="inner">
                             <h3>{{ $total_product }}</h3>
 
-                            <p>Produk</p>
+                            <p>Product</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-bag"></i>
                         </div>
-                        <a href="{{ route('produk.index') }}" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{ route('produk.index') }}" class="small-box-footer">More <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -54,7 +54,7 @@
                         <div class="icon">
                             <i class="ion ion-stats-bars"></i>
                         </div>
-                        <a href="{{ route('talent.index') }}" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{ route('talent.index') }}" class="small-box-footer">More <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -65,12 +65,12 @@
                         <div class="inner">
                             <h3>{{ $total_admin }}</h3>
 
-                            <p>Jumlah Admin</p>
+                            <p>Total Admin</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-person-add"></i>
                         </div>
-                        <a href="{{ route('pengguna.index') }}" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{ route('pengguna.index') }}" class="small-box-footer">More <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -81,12 +81,12 @@
                         <div class="inner">
                             <h3>{{ $total_kategori }}</h3>
 
-                            <p>Kategori</p>
+                            <p>Category</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-pie-graph"></i>
                         </div>
-                        <a href="{{ route('kategori.index') }}" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{ route('kategori.index') }}" class="small-box-footer">More <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
